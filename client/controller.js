997c2d43f56a1a5a65ad91040d4dcf53bc9f2ce0
ZolayvarE <ZolayvarECU@gmail.com>
2016-11-10 19:46:57 -0800

    Update ui to use bootstrap

--- client/controller.js
+++ client/controller.js
@@ -2,10 +2,13 @@ var app = angular.module('timeStamper', []);
 
 app.controller('winston', function ($scope) {
 
+  $scope.convert = convertToSeconds;
+
   if (localStorage.currentTimeSheet) {
     $scope.currentTimeSheet = JSON.parse(localStorage.currentTimeSheet);
   } else {
     $scope.currentTimeSheet = {
+      title: prompt('What would you like to call this timesheet?') || 'Title',
       stamps: [],
       startTime: Date.now(),
     };
@@ -15,45 +18,16 @@ app.controller('winston', function ($scope) {
 
   $scope.textTag = '';
 
-  $scope.convertTime = function (time) {
-
-    var seconds = Math.ceil(time / 1000);
-    var minutes = 0;
-    var hours = 0;
-
-    if (seconds >= 60) {
-      minutes = Math.floor(seconds / 60);
-      seconds = Math.floor(seconds % 60);
-    }
-
-    if (minutes >= 60) {
-      hours = Math.floor(minutes / 60);
-      minutes = Math.floor(minutes % 60);
-    }
-
-    if (hours >= 24) {
-      days = Math.floor(hours / 24);
-      hours = Math.floor(hours % 24);
-    }
-
-    seconds < 10 ? seconds = '0' + seconds : seconds = '' + seconds;
-    minutes < 10 ? minutes = '0' + minutes : minutes = '' + minutes;
-    hours < 10 ? hours = '0' + hours : hours = '' + hours;
-
-    return hours + ':' + minutes + ':' + seconds;
-
-  };
-
-  $scope.currentTime = $scope.convertTime(Date.now() - $scope.currentTimeSheet.startTime);
+  $scope.currentTime = convertToSeconds(Date.now() - $scope.currentTimeSheet.startTime);
 
   $scope.updateTime = function () {
 
-    $scope.currentTime = $scope.convertTime(Date.now() - $scope.currentTimeSheet.startTime);
+    $scope.currentTime = convertToSeconds(Date.now() - $scope.currentTimeSheet.startTime);
     $scope.$apply();
 
   };
 
-  setInterval($scope.updateTime.bind(this), 1000);
+  setInterval($scope.updateTime, 1000);
 
   $scope.startTimer = function () {
 
@@ -66,7 +40,7 @@ app.controller('winston', function ($scope) {
     if ($scope.textTag) {
 
       $scope.currentTimeSheet.stamps.push({
-        time: $scope.convertTime(Date.now() - $scope.currentTimeSheet.startTime),
+        time: Date.now() - $scope.currentTimeSheet.startTime,
         text: $scope.textTag,
       });
 
@@ -84,17 +58,19 @@ app.controller('winston', function ($scope) {
 
     if ($scope.currentTimeSheet.stamps.length) {
       confirmation = confirm(
-        'Are you sure you want to reset your timesheet? \
-        All existing timestamps will be lost.' 
+        [
+        
+          'Are you sure you want to reset your timesheet?',
+          'All existing timestamps will be lost.'
+
+        ].join(' ')
       );
     }
 
     if (confirmation) {
 
-      $scope.currentTimeSheet = $scope.currentTimeSheet = {
-        stamps: [],
-        startTime: Date.now(),
-      };
+      $scope.currentTimeSheet.stamps = [];
+      $scope.currentTimeSheet.startTime = Date.now();
 
       localStorage.currentTimeSheet = JSON.stringify($scope.currentTimeSheet);
 
---
+++ client/scripts/timeConversions.js
@@ -0,0 +1,35 @@
+var convertToSeconds = function (time) {
+
+  var seconds = Math.ceil(time / 1000);
+  var minutes = 0;
+  var hours = 0;
+
+  if (seconds >= 60) {
+    minutes = Math.floor(seconds / 60);
+    seconds = Math.floor(seconds % 60);
+  }
+
+  if (minutes >= 60) {
+    hours = Math.floor(minutes / 60);
+    minutes = Math.floor(minutes % 60);
+  }
+
+  if (hours >= 24) {
+    days = Math.floor(hours / 24);
+    hours = Math.floor(hours % 24);
+  }
+
+  seconds < 10 ? seconds = '0' + seconds : seconds = '' + seconds;
+  minutes < 10 ? minutes = '0' + minutes : minutes = '' + minutes;
+  hours < 10 ? hours = '0' + hours : hours = '' + hours;
+
+  return hours + ':' + minutes + ':' + seconds;
+
+};
+
+var convert = convertToSeconds;
+
+
+
+
+
